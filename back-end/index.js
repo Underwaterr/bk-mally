a160fdc02ce7cbae81fc1fd02058ea214ccf42b1
Tyler Lane <sld.potato@gmail.com>
2020-01-22 11:34:54 -0800

    🦆

---
+++ back-end/index.js
@@ -0,0 +1,18 @@
+let express = require('express')
+let app = express()
+app.use(express.json())
+
+app.use((request, response, next)=> {
+  response.header("Access-Control-Allow-Origin", "*")
+  next()
+})
+
+app.get('/cat', (request, response)=> {
+  response.json([
+    { name: "Fluffy", isCute: true },
+    { name: "Whiskers", isCute: true },
+    { name: "Sox", isCute: true }
+  ])
+})
+
+app.listen(9000, ()=> { console.log("Listening on port 9000")})
---
+++ front-end/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="beer in beers"> {{beer.name}} </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      beers: []
+    }
+  },
+  created() {
+    fetch('https://api.punkapi.com/v2/beers')
+      .then(response=> response.json())
+      .then(data=> this.beers = data)
+  }
+}
+</script>
